0bffb92a47083b900d0133355952845ca5d7546a
Sebastian Ozimkowski <sebastianozimkowski@outlook.com>
2022-05-08 17:59:26 +0200

    123

---
+++ backend/tests/conftest.py
@@ -0,0 +1,17 @@
+from typing import AsyncGenerator, Generator
+
+import pytest
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
+from app.main import app
+
+
+@pytest.fixture()
+def client() -> Generator:
+    yield TestClient(app)
+
+@pytest.fixture()
+async def async_client() -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url="http://0.0.0.0") as ac:
+        yield ac
