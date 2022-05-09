0ca1b3f97341f622ff68bbff2eaf3cd0adf4fb70
Sebastian Ozimkowski <sebastianozimkowski@outlook.com>
2022-05-09 14:59:54 +0200

    123

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/root")
 async def read_root():
     return {"API status": "Hello World!"}
+
+
+@app.get("/123")
+async def read_root():
+    return {"API status": "Hello World!"}
